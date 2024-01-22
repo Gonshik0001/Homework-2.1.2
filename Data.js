70c15acbfb5fb8e2263e859615d26f5c0b133246
Gonshik0001 <132338931+Gonshik0001@users.noreply.github.com>
2024-01-22 00:29:37 -0500

    Updated Data.js

--- Data.js
+++ Data.js
@@ -1,6 +1,9 @@
 let firstName = 'John';
 let age = 43;
-console.log(firstName + ' ' + age);
-console.log (`His name is ${firstName}` )
-typeof {firstNamename:'John', age:43}
-typeof undefined
+
+//console.log(firstName + ' ' + age);
+//console.log (`His name is ${firstName}` );
+let a='John'
+console.log (typeof a);
+let b=43
+console.log (typeof b);
